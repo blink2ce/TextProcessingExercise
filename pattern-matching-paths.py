c5a9127752a6d4ad9f468d618203ffc7720e8c37
Brian Neaves <brianneaves@brians-air.attlocal.net>
2017-10-23 12:07:21 -0500

    Writing breakTies function

--- pattern-matching-paths.py
+++ pattern-matching-paths.py
@@ -7,43 +7,65 @@ numPatterns = int(f.readline())
 patterns = []
 for n in range(0, numPatterns):
 	patterns.append(f.readline().strip().split(','))
-#print(patterns)
+
 
 #Read paths into the program
 numPaths = int(f.readline())
 paths = []
 for n in range(0, numPaths):
 	item = f.readline().strip()
+	#remove slashes from both ends, if they exist
 	if not item[0].isalnum():
 		item = item[1:]
 	if not item[len(item) - 1].isalnum():
 		item = item[:len(item) - 1]
 	item = item.split('/')
 	paths.append(item)
-#print(paths)
+
+
+# def breakTies(matchesList):
+# 	if matchesList[0] == "*":
+# 		del matchesList[0]
+# 		breakTies(matchesList)
+# 	else:
+# 		return matchesList[0]
+
+def breakTies(matchesList):
+	print 'matchesList is'
+	print matchesList
+	index = 0
+	if matchesList[index] != "*" and breakTies(matchesList[index + 1]) == "*":
+		return matchesList[index]
+	else:
+		index = index + 1
 
 def findBestMatch(matchesList):
 	#first remove matches that have more wildcards than any other.
 	countWildcards = []
-	#Make a list that has the count of asterisks of each match.
 	for pattern in matchesList:
 		count = 0
 		for item in pattern:
 			if item == "*":
 				count = count + 1
 		countWildcards.append(count)
-	#remove matches that have more asterisks than another.
 	mostWildcards = max(countWildcards)
 	minWildcards = min(countWildcards)
-	while len(matchesList) > 1  and mostWildcards not == minWildcards:
-		#delete the item where mostWildcards is found and delete 
-		#he corresponding count in countWildcards.
+	while len(matchesList) > 1  and mostWildcards != minWildcards:
+	 	#delete the item where mostWildcards is found and delete 
+	 	#the corresponding count in countWildcards.
+	 	index = countWildcards.index(mostWildcards)
+	 	del countWildcards[index]
+	 	del matchesList[index]
+	 	#Recalculate mostWildcards and minWildcards.
+	 	mostWildcards = max(countWildcards)
+	 	minWildcards = min(countWildcards)
+	 #So, all matches that still exist have the same number of asterisks
+	 #Now it is time to recursively find out which one has a non-wildcard first,
+	 #starting from the left.
+	if len(matchesList) > 1:
+	 	matchesList = breakTies(matchesList)
 
-		#Recalculate mostWildcards and minWildcards.
 
-
-
-	print countWildcards
 
 for path in paths:
 	matchesList = []
@@ -56,7 +78,8 @@ for path in paths:
 						patternMatches = False;
 			if patternMatches:
 				matchesList.append(pattern)
-	findBestMatch(matchesList)
+	if len(matchesList) > 1:
+		findBestMatch(matchesList)
 	if len(matchesList) == 0:
 		matchesList.append("NO MATCH")
 	print matchesList
