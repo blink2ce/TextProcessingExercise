07788dc61cae26e5ca21213cf1fa3c65bd7d0624
Brian Neaves <brianneaves@Brians-MacBook-Air.local>
2017-10-23 20:28:16 -0500

    Broke the program up into functions

--- pattern-matching-paths.py
+++ pattern-matching-paths.py
@@ -1,31 +1,29 @@
 from __future__ import print_function
 import sys
 
-print(sys.argv)
-
-#Open file
-tf = sys.argv[1]
-f = open(tf)
+inputFile = open(sys.argv[1])
 outputFile = sys.argv[2]
 
-#Read patterns into the program
-numPatterns = int(f.readline())
-patterns = []
-for n in range(0, numPatterns):
-	patterns.append(f.readline().strip().split(','))
+def readPatterns():
+	numPatterns = int(inputFile.readline())
+	patterns = []
+	for n in range(0, numPatterns):
+		patterns.append(inputFile.readline().strip().split(','))
+	return patterns
 
-#Read paths into the program
-numPaths = int(f.readline())
-paths = []
-for n in range(0, numPaths):
-	item = f.readline().strip()
-	#remove slashes from both ends, if they exist
-	if not item[0].isalnum():
-		item = item[1:]
-	if not item[len(item) - 1].isalnum():
-		item = item[:len(item) - 1]
-	item = item.split('/')
-	paths.append(item)
+def readPaths():
+	numPaths = int(inputFile.readline())
+	paths = []
+	for n in range(0, numPaths):
+		item = inputFile.readline().strip()
+		#remove slashes from both ends, if they exist
+		if not item[0].isalnum():
+			item = item[1:]
+		if not item[len(item) - 1].isalnum():
+			item = item[:len(item) - 1]
+		item = item.split('/')
+		paths.append(item)
+	return paths
 
 def findBestMatch(matchesList):
 	#first remove matches that have more wildcards than any other.
@@ -48,27 +46,31 @@ def findBestMatch(matchesList):
 	 	mostWildcards = max(countWildcards)
 	 	minWildcards = min(countWildcards)
 
-with open(outputFile, 'w') as f:
-	for path in paths:
-		matchesList = []
-		for pattern in patterns:
-			if len(pattern) == len(path):
-				patternMatches = True;
-				for index in range(0, len(path)):
-					if not pattern[index] == '*':
-						if not path[index] == pattern[index]:
-							patternMatches = False;
-				if patternMatches:
-					matchesList.append(pattern)
-		if len(matchesList) > 1:
-			findBestMatch(matchesList)
-		if len(matchesList) == 0:
-			matchesList.append(["NO MATCH"])
-		#print matchesList
-		for item in matchesList:
-			item = ','.join(item)
-			f.write(item + '\n')
-			print(item)
 
+def main():
+	patterns = readPatterns()
+	paths = readPaths()
+	with open(outputFile, 'w') as f:
+		for path in paths:
+			matchesList = []
+			for pattern in patterns:
+				if len(pattern) == len(path):
+					patternMatches = True;
+					for index in range(0, len(path)):
+						if not pattern[index] == '*':
+							if not path[index] == pattern[index]:
+								patternMatches = False;
+					if patternMatches:
+						matchesList.append(pattern)
+			if len(matchesList) > 1:
+				findBestMatch(matchesList)
+			if len(matchesList) == 0:
+				matchesList.append(["NO MATCH"])
+			#print matchesList
+			for item in matchesList:
+				item = ','.join(item)
+				f.write(item + '\n')
+				print(item)
 
+main()
 
